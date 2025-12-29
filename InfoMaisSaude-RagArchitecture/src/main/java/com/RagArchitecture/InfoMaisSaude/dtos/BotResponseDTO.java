3eca88cfcc16087201a3666b4f3e99da32cac59a
AndreySantos <santosandrey146@gmail.com>
2025-12-29 17:04:38 -0300

    Adicionado correção no atributo botões

--- InfoMaisSaude-RagArchitecture/src/main/java/com/RagArchitecture/InfoMaisSaude/dtos/BotResponseDTO.java
+++ InfoMaisSaude-RagArchitecture/src/main/java/com/RagArchitecture/InfoMaisSaude/dtos/BotResponseDTO.java
@@ -3,25 +3,28 @@ package com.RagArchitecture.InfoMaisSaude.dtos;
 import java.util.List;
 
 public class BotResponseDTO {
-
     private String texto;
     private List<String> botoes;
 
-
     public BotResponseDTO(String texto) {
-        this.texto = texto;
-    }
-    
-    public BotResponseDTO(String texto, List<String> botoes) {
         this.texto = texto;
         this.botoes = null;
     }
 
+    public BotResponseDTO(String texto, List<String> botoes) {
+        this.texto = texto;
+        this.botoes = botoes; 
+    }
 
-   public String getTexto() { return texto; }
-   public List<String> getBotoes() { return botoes; }
-   public boolean temBotoes() { return botoes != null && !botoes.isEmpty(); }
+    public String getTexto() {
+        return texto;
+    }
 
+    public List<String> getBotoes() {
+        return botoes;
+    }
 
-    
+    public boolean temBotoes() {
+        return botoes != null && !botoes.isEmpty();
+    }
 }
