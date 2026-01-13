8c6b69c5e13e2c7cdad14a46ed4bf5ba279e66b0
AndreySantos <santosandrey146@gmail.com>
2026-01-13 09:59:39 -0300

    Adicionando anotação service ao BotAuthentication

--- InfoMaisSaude-RagArchitecture/src/main/java/com/RagArchitecture/InfoMaisSaude/services/BotAuthenticationService.java
+++ InfoMaisSaude-RagArchitecture/src/main/java/com/RagArchitecture/InfoMaisSaude/services/BotAuthenticationService.java
@@ -5,8 +5,10 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Service
 public class BotAuthenticationService {
     
     @Value("${bot.username}") 
